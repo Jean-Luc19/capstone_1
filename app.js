3282425dcf825d15b50c80d1a61e4b002d07f12f
dustwise <jared.mohney@gmail.com>
2017-02-09 11:00:10 -0700

    api working, isolating values we want

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-//code here
+const URL = "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyAoq2H7SrmQO7EeyXNvdwYWXHYYM4Xh0Ms";
+const API_KEY = "AIzaSyAoq2H7SrmQO7EeyXNvdwYWXHYYM4Xh0Ms";
+const appState = {};
+
+
+
+function getData(){
+  const query = {
+    sort: "popularity"
+  };
+
+  $.getJSON(URL, query, handleData);
+}
+
+function handleData({items}){
+  $.each(items, function(index, {family, category, variants}){
+
+    console.log(family + " / " + category + " / " + variants);
+  });
+}
+
+function initializeClickHandlers(){
+  $('#js-container').on("click", "#js-randomizeBtn", function(){
+    getData();
+  });
+}
+
+
+$(function(){
+  initializeClickHandlers();
+});
+
+
+
+
